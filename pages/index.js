734c4651be06c9ffc42948504ecb2cf6953b12e1
Gabriel Correia <gabriel.correia@alicerceedu.com.br>
2022-04-26 18:09:41 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      Hello World.{' '}
+      Hello World. 22{' '}
       <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
         <a>About</a>
       </Link>
